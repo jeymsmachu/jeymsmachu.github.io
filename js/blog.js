80744c7c2464bb1e3f1c2007012ba4186976c53d
jeymsmachu <159496573+jeymsmachu@users.noreply.github.com>
2026-05-24 04:08:13 +0800

    fix (hopefully lol)

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,5 @@
 // helper
-const BASE_PATH = window.location.pathname.includes('/jeymsmachu/') ? 'jeymsmachu/' : '';
-
+const BASE_PATH = window.location.pathname.includes('/jeymsmachu/') ? '/jeymsmachu/' : '/';
 
 // parse front matter from markdown
 function parseFrontMatter(markdown) {
